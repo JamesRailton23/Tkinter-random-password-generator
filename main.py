be8150684d1d15192c6629d9236abb5b8b3093ba
James Railton <jamesrailton2003@gmail.com>
2023-09-11 12:04:11 +0100

    Update 2.5 multiple character settings did not work so had to remove

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ def clear_text():
 
 def random_password_generator():
     password_length = int(slider_password_length.get())
-    allow_uppercase = bool_upper_characters.get()
-    allow_numbers = bool_numbers_checkbox.get()
-    allow_special_characters = bool_special_characters.get()
 
     password = ""
     shuffled_password = ""
@@ -33,45 +30,17 @@ def random_password_generator():
     special_characters = ["!", "£", "$", "%", "^", "&", "*", "(", ")", "_", "-", "[", "]", "{", "}", "@", "?", "/", ".",
                           "<", ">", "#", "`", "¬", ";", "+", "="]
     # Generates the full random password
-    if allow_uppercase and allow_numbers and allow_special_characters:
-        for i in range(password_length):
-            selector = random.randint(1, 4)
-            if selector == 1:  # characters
-                password += random.choice(characters)
-            elif selector == 2:  # upper case letters
-                upper_character = random.choice(characters)
-                password += str(upper_character).upper()
-            elif selector == 3:  # numbers
-                password += random.choice(numbers)
-            elif selector == 4:  # special characters
-                password += random.choice(special_characters)
-
-    # generates without special characters
-    elif allow_uppercase and allow_numbers:
-        for i in range(password_length):
-            selector = random.randint(1, 3)
-            if selector == 1:  # characters
-                password += random.choice(characters)
-            elif selector == 2:  # upper case letters
-                upper_character = random.choice(characters)
-                password += str(upper_character).upper()
-            elif selector == 3:  # numbers
-                password += random.choice(numbers)
-
-    # generates without special characters and numbers
-    elif allow_uppercase:
-        for i in range(password_length):
-            selector = random.randint(1, 2)
-            if selector == 1:  # characters
-                password += random.choice(characters)
-            elif selector == 2:  # upper case letters
-                upper_character = random.choice(characters)
-                password += str(upper_character).upper()
-
-    # generates without special characters, numbers and special characters
-    else:
-        for i in range(password_length):
+    for i in range(password_length):
+        selector = random.randint(1, 4)
+        if selector == 1:  # characters
             password += random.choice(characters)
+        elif selector == 2:  # upper case letters
+            upper_character = random.choice(characters)
+            password += str(upper_character).upper()
+        elif selector == 3:  # numbers
+            password += random.choice(numbers)
+        elif selector == 4:  # special characters
+            password += random.choice(special_characters)
 
     # shuffles and returns random password
     password = list(password)
@@ -91,25 +60,9 @@ window.resizable(False, False)
 # top label
 top_label = Label(window, text="Random Password Generator", font="Arial 20 underline")
 
-# check buttons values
-bool_special_characters = BooleanVar()
-bool_numbers_checkbox = BooleanVar()
-bool_upper_characters = BooleanVar()
-
-# checkbox to either include or remove special characters
-checkbox_special_characters = Checkbutton(window, text="Include special Characters", onvalue=True, offvalue=False,
-                                          height=2, width=25, font="Arial 15", variable=bool_special_characters)
-
-# checkbox to either include or remove numbers
-checkbox_numbers = Checkbutton(window, text="Include numbers", onvalue=True, offvalue=False, height=2, width=20,
-                               font="Arial 15", variable=bool_numbers_checkbox)
-
-# checkbox to either include or remove upper case letters
-checkbox_uppercase_characters = Checkbutton(window, text="Include uppercase characters", onvalue=True, offvalue=False,
-                                            height=2, width=25, font="Arial 15", variable=bool_upper_characters)
-
 # password length counter
-slider_password_length = Scale(window, from_=0, to=100, orient=HORIZONTAL, length=470, tickinterval=5)
+slider_password_length = Scale(window, from_=0, to=100, orient=HORIZONTAL, length=470, tickinterval=5,
+                               label="Select how many characters you want in your password", relief=GROOVE)
 
 # generate password button
 button_generate_password = Button(window, text="Generate Password", height=2, width=20, font=10,
@@ -123,9 +76,6 @@ button_copy_to_clipboard = Button(window, text="Copy to clipboard", height=2, wi
 
 # layout grid
 top_label.pack(pady=30)  # top Label
-checkbox_special_characters.pack()  # character checkboxes
-checkbox_numbers.pack()  # character checkboxes
-checkbox_uppercase_characters.pack()  # character checkboxes
 slider_password_length.pack(pady=20)  # character amount slider
 slider_password_length.set(16)  # sets default character value
 button_generate_password.pack(pady=20)  # Generate password button
